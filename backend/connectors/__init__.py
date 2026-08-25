e751866bdd9b8ebd814d9a25b86b1338065f4360
amirmohd4 <168186457+amirmohd4@users.noreply.github.com>
2026-08-25 11:28:47 +0530

    Update __init__.py

--- backend/connectors/__init__.py
+++ backend/connectors/__init__.py
@@ -1,7 +1,7 @@
 from .base_connector import BaseConnector
 from .karnataka_connectors import KaveriConnector, EAssthiConnector, BhoomiConnector
 from .jk_connectors import LRISConnector
-from .generic_connectors import AadhaarConnector, DigiLockerConnector
+from .generic_connectors import AadhaarConnector, DigilockerConnector
 from .municipal_connector import MunicipalZoningConnector
 from .court_connector import CourtRecordsConnector
 
@@ -12,7 +12,7 @@ __all__ = [
     "BhoomiConnector",
     "LRISConnector",
     "AadhaarConnector",
-    "DigiLockerConnector",
+    "DigilockerConnector",
     "MunicipalZoningConnector",
     "CourtRecordsConnector"
 ]
