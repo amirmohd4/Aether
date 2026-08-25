822c286ea2f7d27822c3959d5e6d26685691af1b
amirmohd4 <168186457+amirmohd4@users.noreply.github.com>
2026-08-25 11:36:36 +0530

    Update __init__.py

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,13 +1,14 @@
 # Services package
-from ..config import settings   # <-- Add two dots to go up one folder
 from .cache_service import cache
 from .connector_service import connector_service
 from .workflow_service import workflow_engine
 from .fraud_service import fraud_service
+from .billing_service import billing_service
 
 __all__ = [
     "cache",
     "connector_service",
     "workflow_engine",
-    "fraud_service"
+    "fraud_service",
+    "billing_service"
 ]
